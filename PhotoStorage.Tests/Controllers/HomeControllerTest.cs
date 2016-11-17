2bfe9f050ac66790d444d84747d9ef5445941b52
jsabados <janko.sabados@wolterskluwer.com>
2016-11-17 11:04:21 +0000

    Added unit tests

---
+++ PhotoStorage.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PhotoStorage.Controllers;
+using System.Web.Mvc;
+using System.Threading.Tasks;
+
+namespace PhotoStorage.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index() {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            Task<ActionResult> result = controller.Index() as Task<ActionResult>;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
